cea3c09deffbb4bd2995379bbe095f30622b7e25
webberbond <sergey.zrch@gmail.com>
2023-04-20 22:03:55 +0500

    Edited Model

--- Qase/Entities/Models/TestProjectModel.cs
+++ Qase/Entities/Models/TestProjectModel.cs
@@ -15,27 +15,18 @@ public class TestProjectModel
 
         var other = (TestProjectModel)obj;
 
-        if (ProjectName != other.ProjectName)
-        {
-            throw new AssertionException($"Project name does not match. Expected: {ProjectName}, Actual: {other.ProjectName}");
-        }
-
-        if (ProjectCode != other.ProjectCode)
-        {
-            throw new AssertionException($"Project code does not match. Expected: {ProjectCode}, Actual: {other.ProjectCode}");
-        }
-
-        if (ProjectDescription != other.ProjectDescription)
-        {
-            throw new AssertionException($"Project description does not match. Expected: {ProjectDescription}, Actual: {other.ProjectDescription}");
-        }
-
-        return true;
+        return ProjectName == other.ProjectName &&
+               ProjectCode == other.ProjectCode &&
+               ProjectDescription == other.ProjectDescription;
     }
 
     public override int GetHashCode()
     {
         return HashCode.Combine(ProjectName, ProjectCode, ProjectDescription);
     }
-}
 
+    public override string ToString()
+    {
+        return $"TestProjectModel: ProjectName='{ProjectName}', ProjectCode='{ProjectCode}', ProjectDescription='{ProjectDescription}'";
+    }
+}
